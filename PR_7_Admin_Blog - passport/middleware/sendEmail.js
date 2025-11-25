42eb777e17e2851b64cf5cbac056c48572c0446a
Pallavisorathiya <pallavisorathiya23@gmail.com>
2025-11-25 23:13:55 +0530

    Project_7

---
+++ PR_7_Admin_Blog - passport/middleware/sendEmail.js
@@ -0,0 +1,19 @@
+const nodemailer =require('nodemailer');
+
+
+exports.sendEmail = async(msg) => {
+
+    let transporter = nodemailer.createTransport({
+       service: 'gmail',
+        port: 587,
+        secure: false, 
+        auth: {
+            user: "pallavisorathiya23@gmail.com",
+            pass: "ynxisqnxtyxqcfjr",
+        },
+    })
+
+    const info = await transporter.sendMail(msg)
+    console.log("Message sent:", info.messageId);
+    return info;
+}
